70968683390e4449bc9ed7d912b942560eea93bf
Lean Albuquerque <lean.albuquerque@mail.uft.edu.br>
2025-07-08 09:50:54 -0300

    fixed bug with imports

--- agente_ia.py
+++ agente_ia.py
@@ -1,6 +1,6 @@
 import os
 import google.generativeai as genai
-from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeA
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
